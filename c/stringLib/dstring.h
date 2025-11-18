3a4a3585de6db318d24d357293812c6a645bcea4
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-11-18 17:45:45 +0100

    Fix string library API and test issues
    Changed function signatures to use double pointers for dstring_t parameters, allowing functions to modify strings with changes persisting outside the function scope. Created proper test file with better coverage. Several functions still have logic errors that need to be addressed in the next iteration.

--- c/stringLib/dstring.h
+++ c/stringLib/dstring.h
@@ -9,13 +9,13 @@ typedef struct {
 } dstring_t;
 
 dstring_t* create_string(const char *init);
-void destroy_string(dstring_t *str);
-void string_append(dstring_t *str, const char *text);
-void string_prepend(dstring_t *str, const char *text);
-dstring_t* substring(dstring_t *src, size_t start, size_t len);
-int find_string(dstring_t *src, const char *text);
-int string_cmp(dstring_t *src, const char *text);
-void print_string(dstring_t *str);
+void destroy_string(dstring_t **str);
+void string_append(dstring_t **str, const char *text);
+void string_prepend(dstring_t **str, const char *text);
+dstring_t* substring(dstring_t **src, size_t start, size_t len);
+int find_string(dstring_t **src, const char *text);
+int string_cmp(dstring_t **src, const char *text);
+void print_string(dstring_t **str);
 size_t str_len(const char *text);
 
 #endif
