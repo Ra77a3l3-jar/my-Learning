88cc452708997100f47daf0e5f121a564f442fa0
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2025-11-18 17:45:45 +0100

    Applying chages to resolve some bugs
    - Right now i changed to a double pointer for strings in input of type
    dstring_t so i can modify them and still have the modifications outside
    the function block.
    - Made a correct test file.
    - There are still multiple functions that have a wrong logic.

--- c/stringLib/dstring.c
+++ c/stringLib/dstring.c
@@ -4,13 +4,13 @@
 
 #define STR_LEN_DEF 20
 
-size_t str_len(const char *str) {
-    const char *x = str;
+size_t str_len(const char *text) {
+    const char *x = text;
     // Traverses to the last char
     while(*x) {
         x++;
     }
-    return (x - str);
+    return (x - text);
 }
 
 dstring_t* create_string(const char *init) {
@@ -59,84 +59,85 @@ dstring_t* create_string(const char *init) {
     return str;
 }
 
-void destroy_string(dstring_t *str) {
-    if(str) {
-        if(str->data) {
-            free(str->data);
+void destroy_string(dstring_t **str) {
+    if(str && *str) {
+        if((*str)->data) {
+            free((*str)->data);
         }
-        free(str);
+        free(*str);
+        *str = NULL;
     }
 }
 
-void string_append(dstring_t *str, const char *text) {
-    if(!str || !text) return;
+void string_append(dstring_t **str, const char *text) {
+    if(!*str || !text) return;
     
     size_t text_len = str_len(text);
-    size_t new_length = str->length + text_len;
+    size_t new_length = (*str)->length + text_len;
     
     // Check if we need to resize
-    if(new_length + 1 > str->capacity) {
+    if(new_length + 1 > (*str)->capacity) {
         size_t new_capacity = (new_length + 1) * 2;
-        char *new_data = realloc(str->data, new_capacity);
+        char *new_data = realloc((*str)->data, new_capacity);
         if(!new_data) {
             printf("Memory allocation failed!\n");
             return;
         }
-        str->data = new_data;
-        str->capacity = new_capacity;
+        (*str)->data = new_data;
+        (*str)->capacity = new_capacity;
     }
     
     // Copy text to end
     size_t i;
     for(i = 0; i < text_len; i++) {
-        str->data[str->length + i] = text[i];
+        (*str)->data[(*str)->length + i] = text[i];
     }
-    str->data[new_length] = '\0';
-    str->length = new_length;
+    (*str)->data[new_length] = '\0';
+    (*str)->length = new_length;
 }
 
-void string_prepend(dstring_t *str, const char *text) {
-    if(!str || !text) return;
+void string_prepend(dstring_t **str, const char *text) {
+    if(!*str || !text) return;
     
     size_t text_len = str_len(text);
-    size_t new_length = str->length + text_len;
+    size_t new_length = (*str)->length + text_len;
     
     // Check if we need to resize
-    if(new_length + 1 > str->capacity) {
+    if(new_length + 1 > (*str)->capacity) {
         size_t new_capacity = (new_length + 1) * 2;
-        char *new_data = realloc(str->data, new_capacity);
+        char *new_data = realloc((*str)->data, new_capacity);
         if(!new_data) {
             printf("Memory allocation failed!\n");
             return;
         }
-        str->data = new_data;
-        str->capacity = new_capacity;
+        (*str)->data = new_data;
+        (*str)->capacity = new_capacity;
     }
     
     // Shift existing string to the right
     // Shifts starting from the last char of the original string
     size_t i;
-    for(i = str->length; i > 0; i--) {
-        str->data[i + text_len - 1] = str->data[i - 1];
+    for(i = (*str)->length; i > 0; i--) {
+        (*str)->data[i + text_len - 1] = (*str)->data[i - 1];
     }
     
     // Copy text to beginning
     for(i = 0; i < text_len; i++) {
-        str->data[i] = text[i];
+        (*str)->data[i] = text[i];
     }
-    str->data[new_length] = '\0';
-    str->length = new_length;
+    (*str)->data[new_length] = '\0';
+    (*str)->length = new_length;
 }
 
 // This function it returns a substring give the start index and the lenght of the substring
-dstring_t* substring(dstring_t *src, size_t start, size_t len) {
-    if(!src || start >= src->length) {
+dstring_t* substring(dstring_t **src, size_t start, size_t len) {
+    if(!*src || start >= (*src)->length) {
         return create_string("");
     }
     
     // Adjust length if it goes beyond string end
-    if(start + len > src->length) {
-        len = src->length - start;
+    if(start + len > (*src)->length) {
+        len = (*src)->length - start;
     }
     
     // Create temporary buffer for substring
@@ -148,7 +149,7 @@ dstring_t* substring(dstring_t *src, size_t start, size_t len) {
     
     size_t i;
     for(i = 0; i < len; i++) {
-        temp[i] = src->data[start + i];
+        temp[i] = (*src)->data[start + i];
     }
     temp[len] = '\0';
     
@@ -158,18 +159,18 @@ dstring_t* substring(dstring_t *src, size_t start, size_t len) {
     return result;
 }
 
-int find_string(dstring_t *src, const char *text) {
+int find_string(dstring_t **src, const char *text) {
     // Check for border case
-    if(!src || !text) return -1;
+    if(!*src || !text) return -1;
 
     size_t text_len = str_len(text);
-    if(text_len == 0 || text_len > src->length) return -1;
+    if(text_len == 0 || text_len > (*src)->length) return -1;
     
     size_t i = 0, j = 0;
-    for(i = 0; i <= src->length - text_len; i++) {
+    for(i = 0; i <= (*src)->length - text_len; i++) {
         int match = 1;
         for(j = 0; j < text_len; j++) {
-            if(src->data[i + j] != text[j]) {
+            if((*src)->data[i + j] != text[j]) {
                 match = 0;
                 break;
             }
@@ -182,22 +183,22 @@ int find_string(dstring_t *src, const char *text) {
     return -1;
 }
 
-int string_cmp(dstring_t *src, const char *text) {
-    if(!src || !text) return -2;
+int string_cmp(dstring_t **src, const char *text) {
+    if(!*src || !text) return -2;
     
     size_t i;
-    for(i = 0; i < src->length && text[i] != '\0'; i++) {
-        if(src->data[i] < text[i]) return -1;
-        if(src->data[i] > text[i]) return 1;
+    for(i = 0; i < (*src)->length && text[i] != '\0'; i++) {
+        if((*src)->data[i] < text[i]) return -1;
+        if((*src)->data[i] > text[i]) return 1;
     }
     
-    if(i < src->length) return 1;   // src is longer
+    if(i < (*src)->length) return 1;   // src is longer
     if(text[i] != '\0') return -1;  // text is longer
     return 0;                        // equal
 }
 
-void print_string(dstring_t *str) {
-    if(str && str->data) {
-        printf("%s", str->data);
+void print_string(dstring_t **str) {
+    if(*str && (*str)->data) {
+        printf("%s\n", (*str)->data);
     }
 }
--- c/stringLib/testString.c
+++ c/stringLib/testString.c
@@ -1,25 +1,60 @@
 #include "dstring.h"
 
+#include <stdio.h>
+
 int main(void) {
 
     dstring_t *str = create_string("Hello, World!");
 
-    string_append(str, " by Raffaele");
-    print_string(str);
+    string_append(&str, " by Raffaele");
+    print_string(&str);
 
     dstring_t *str1 = create_string("combo col Gigante.");
-    string_prepend(str1, "Solo ");
+    string_prepend(&str1, "Solo ");
 
-    dstring_t *sub = substring(str1, 2, 3);
-    print_string(sub);
+    dstring_t *sub = substring(&str1, 11, 3);
+    print_string(&sub);
     
-    find_string(str1, "Gigante");
+    int i = find_string(&str1, "Gigante");
+    if(i != -1) {
+        printf("The string is present (index %d).\n", i);
+    } else {
+        printf("The string is not present.\n");
+    }
 
     dstring_t *str2 = create_string("Batman");
-    string_cmp(str2, "Batman");
-    string_cmp(str2, "batman");
+    int x = string_cmp(&str2, "Batman");
+    if(x == 0) {
+        printf("The strings are equal.\n");
+    } else if(x == -1) {
+        printf("The text is longer.\n");
+    } else if(x == 1) {
+        printf("The source is longer.\n");
+    } else if(x == -2) {
+        printf("One or both strings are NULL.\n");
+    }
 
-    print_string(str1);
+    int y = string_cmp(&str2, "batman");
+    if(y == 0) {
+        printf("The strings are equal.\n");
+    } else if(y == -1) {
+        printf("The text is longer.\n");
+    } else if(y == 1) {
+        printf("The source is longer.\n");
+    } else if(y == -2) {
+        printf("One or both strings are NULL.\n");
+    }
 
-    str_len("Hello, World!");
+    print_string(&str1);
+
+    size_t leng = str_len("Hello, World!");
+    printf("The length of the string is %zu.\n", leng);
+
+    // free allocated strings
+    destroy_string(&str);
+    destroy_string(&str1);
+    destroy_string(&sub);
+    destroy_string(&str2);
+
+    return 0;
 }
